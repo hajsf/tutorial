eb65816a2f241bc8b5abd303b3ae66d24d41c926
Hasan Yousef <hyousef@msn.com>
2022-01-25 22:43:23 +0300

    cgo inline

--- ffi/main.c
+++ ffi/main.c
@@ -1,7 +1,7 @@
 #include  <stdio.h>
 int Add(int a, int b){
     printf("Welcome from external C function\n");
-    return a+b;
+    return a + b;
 }
 
 // clang -shared -fpic main.c -o libhello.dylib
