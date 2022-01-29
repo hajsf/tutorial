bb34e0b28470c3c91c5ce3ddf03dd76e0ab9b739
Hasan Yousef <hyousef@msn.com>
2022-01-29 23:00:38 +0300

    embed

--- embed/main.go
+++ embed/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"strings"
 
-	_ "embed"
+	_ "embed" // the _ used to enforce import, as GO compiler will understand there is unused import,
 )
 
 var Version string = strings.TrimSpace(version)
 
 //go:embed version_prod.go
 var src string // embed the source code of the version_prod.go
+// if the embeded item is a folder, then files started with . or _ are not embeded
 
 func main() {
 	fmt.Printf("Version %q\n", Version)
