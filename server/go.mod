1a1ce9aa74d3250763d12fc7d4c4ee085ff89661
Hasan Yousef <hyousef@msn.com>
2022-01-29 22:48:33 +0300

    server

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.io/hajsf/server
+
+go 1.18
---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	useOS := len(os.Args) > 1 && os.Args[1] == "live"
+	http.Handle("/", http.FileServer(getFileSystem(useOS)))
+	http.ListenAndServe(":8888", nil)
+}
+
+//go:embed static
+var embededFiles embed.FS
+
+func getFileSystem(useOS bool) http.FileSystem {
+	if useOS {
+		log.Print("using live mode")
+		return http.FS(os.DirFS("static"))
+	}
+
+	log.Print("using embed mode, open: http://localhost:8888/index.html")
+	fsys, err := fs.Sub(embededFiles, "static")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
+
+// $ go run github.io/hajsf/server
+// open: http://localhost:8888/index.html
