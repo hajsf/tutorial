0c41ae0c7fb4ab4c3ab31e93e09ad535a0169349
hajsf <98168280+hajsf@users.noreply.github.com>
2022-06-14 10:41:06 +0300

    Add files via upload

---
+++ Push QR from server to client/go.mod
@@ -0,0 +1,16 @@
+module whatsapp
+
+go 1.18
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.13
+	go.mau.fi/whatsmeow v0.0.0-20220604174810-f28d984f1b9a
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	go.mau.fi/libsignal v0.0.0-20220425070825-c40c839ee6a0 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+)
---
+++ Push QR from server to client/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/protobuf/proto"
+
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+)
+
+func eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		fmt.Println("Received a message!", v.Message.GetConversation())
+	}
+}
+
+func main() {
+	passer := &DataPasser{logs: make(chan string)}
+
+	http.HandleFunc("/sse/dashboard", passer.handleHello)
+	go http.ListenAndServe(":1234", nil)
+	/*
+		ticker := time.NewTicker(500 * time.Millisecond)
+		defer ticker.Stop()
+		done := make(chan bool)
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				case <-ticker.C:
+					//	fmt.Println("Tick at", t)
+					// passer.logs <- buffer.String()
+				}
+			}
+		}()
+	*/
+	store.DeviceProps.Os = proto.String("WhatsApp GO")
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:datastore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client.AddEventHandler(eventHandler)
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+
+		for evt := range qrChan {
+			switch evt.Event {
+			case "success":
+				{
+					passer.logs <- "success"
+					fmt.Println("Login event: success")
+				}
+			case "timeout":
+				{
+					passer.logs <- "timeout"
+					fmt.Println("Login event: timeout")
+				}
+			case "code":
+				{
+					fmt.Println("new code recieved")
+					fmt.Println(evt.Code)
+					passer.logs <- evt.Code
+				}
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		passer.logs <- "Already logged"
+		fmt.Println("Already logged")
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
